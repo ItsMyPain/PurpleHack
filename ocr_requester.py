ab37c512124ed59f181bfe4ee375fb66cc0d83b4
cnrprod-team-63144 <maxxxi_boyk@mail.ru>
2024-03-10 21:03:36 +0300

    text

--- ocr_requester.py
+++ ocr_requester.py
@@ -1,7 +1,8 @@
 import os
-
+from pathlib import Path
 import requests
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -24,8 +25,24 @@ def ocr_space_file(filename):
                           files={filename: f},
                           data=payload,
                           )
-    return r.content.decode()
+    return json.loads(r.content.decode())
+
+def save_result(text: str, file_name: str = 'result.txt'):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        f.write(text.replace('\n', ''))
+
+def get_text_from_pdf(folder: Path, pdf_id: int = 0, part: int = 0):
+    file_path = folder / f'id_{pdf_id}' / f'id_{pdf_id}_{part}.pdf'
+    test_file = ocr_space_file(filename=str(file_path))
+    text = ''
+    for el in test_file['ParsedResults']:
+        text += el['ParsedText']
+    return text
 
 
-test_file = ocr_space_file(filename='0_part_0.pdf')
-print(test_file)
+if __name__ == '__main__':
+    pdf_folder = Path(r'C:\Users\maxxx\VSprojects\hacks\purple\download')
+    text = get_text_from_pdf(pdf_folder, pdf_id=0, part=0)
+    print(text)
+    save_result(text)
+
