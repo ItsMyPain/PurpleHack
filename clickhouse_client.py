70c1e191322032a0b29d2a4d333752a83940ae60
Sergey <agibalov1294@gmail.com>
2024-03-10 21:37:23 +0300

    added new library for work with clickhouse

--- clickhouse_client.py
+++ clickhouse_client.py
@@ -1,22 +1,51 @@
 import os
 
+import clickhouse_connect
+import numpy as np
 from dotenv import load_dotenv
-from infi.clickhouse_orm import Model, StringField, Float32Field, MergeTree, ArrayField, Database, UInt16Field
 
 load_dotenv()
 
 
-class Document(Model):
-    __tablename__ = 'document'
+class ClickHouse:
+    def __init__(self):
+        self.client = clickhouse_connect.get_client(host=os.getenv('CH_HOST'), port=int(os.getenv('CH_PORT')),
+                                                    username=os.getenv('CH_USER'), password=os.getenv('CH_PASS'))
 
-    id = UInt16Field()
-    title = StringField()
-    embedding = ArrayField(Float32Field())
+    def create_table(self):
+        self.client.command("""
+        CREATE TABLE document( 
+            url String,
+            embedding Array(Float32),
+            paragraph String,
+            text String
+        )
+        ENGINE = MergeTree()
+        ORDER BY embedding;
+        """)
 
-    engine = MergeTree(partition_key=['id'], order_by=['id'])
+    def insert(self, data:list[list]):
+        self.client.insert('document', data, column_names=['url', 'embedding', 'paragraph', 'text'])
+
+    def select(self, data: list[float]):
+        return self.client.query("""
+        SELECT url, paragraph, text, cosineDistance({data:Array(Float32)}, embedding) AS score 
+        FROM document 
+        ORDER BY score DESC 
+        LIMIT 10
+        """, parameters={'data': data})
 
 
-db = Database(os.getenv('CH_DATABASE'), db_url=os.getenv('CH_URL'), username=os.getenv('CH_USER'),
-              password=os.getenv('CH_PASS'))
-db.drop_table(Document)
-db.create_table(Document)
+if __name__ == '__main__':
+    ch = ClickHouse()
+    # ch.create_table()
+
+    # input_data = []
+    # input_emb = np.load('test.npy')
+    # for i in input_emb:
+    #     input_data.append(['test_url', i, 'test_paragraph', 'test_text'])
+    # ch.insert(input_data)
+
+    input_query = np.load('test_q.npy')[0].tolist()
+    output = ch.select(input_query)
+    print(output.result_rows)
